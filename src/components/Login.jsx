46d24a92dd2410d0b6d5269d99a4d834354ee1cb
RolandoMamani <rolomc2016@gmail.com>
2025-11-06 09:36:27 -0500

    login google

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+//Proveedor de Google
+const provider = new GoogleAuthProvider();
+
 
 function Login({ onLogin, cambiarVista}) {
+
+    //Iniciar sesion con email y password
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -20,6 +25,28 @@ function Login({ onLogin, cambiarVista}) {
             console.log("Error al iniciar sesion");
         });
     }
+    //--------------------------------------------------
+
+    const iniciarSesionGoogle = () => {
+        // Documentacion de Firebase
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+            console.log("Iniciaste sesion con Google");
+            onLogin(user);
+            
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log("Error al iniciar con Google");
+            console.log(error);
+        });
+    }
 
     return(
         <div>
@@ -35,6 +62,7 @@ function Login({ onLogin, cambiarVista}) {
                 onChange={(e) => setPassword(e.target.value)}
             />
             <button onClick={iniciarSesion}>Iniciar Sesion</button>
+            <button onClick={iniciarSesionGoogle}>Iniciar Sesion con Google</button>
             <p>No tienes una cuenta?</p>
             <button onClick={cambiarVista}>Crear cuenta</button>
         </div>
